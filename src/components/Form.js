71b7966eab0f945ba76db02c97369a45e032a84d
Umesh <uhavaiya@gmail.com>
2021-08-12 11:31:41 +0530

    form21tut

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,22 +5,57 @@ class Form extends Component {
         super(props)
 
         this.state = {
-            username: ''
+            username: '',
+            comments: '',
+            topic: 'react'
         }
     }
-    handlerUsernameChange = (event) => {
+    handlerUsernameChange = event => {
         this.setState({
             username: event.target.value
         })
     }
+
+    handleCommentsChange = event => {
+        this.setState({
+            comments: event.target.value
+        })
+    }
+
+    handlerTopicChange = event => {
+        this.setState({
+            topic: event.target.value
+        })
+    }
+    handleSubmit = event =>{
+        alert(`${this.state.username} ${this.state.comments} ${this.state.topic}`)
+        event.preventDefault()
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div>
                     <label>Username</label>
                     <input type='text'
                         value={this.state.username}
-                        onChange={this.handlerUsernameChange} />
+                        onChange={this.handlerUsernameChange}
+                    />
+                </div>
+                <div>
+                    <label>Comments</label>
+                    <textarea value={this.state.comments} onChange={this.handleCommentsChange}></textarea>
+                </div>
+                <div>
+                    <label>Topic: </label>
+                    <select value={this.state.topic} onChange={this.handlerTopicChange}>
+                        <option value = "react">React</option>
+                        <option value = "angular">Angular</option>
+                        <option value = "vue">Vue</option>
+                    </select>
+                </div>
+                <div>
+                    <button type="submit">Submit</button>
                 </div>
             </form>
         )
